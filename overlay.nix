704f8eb08cf92c23ea4920bc70d504d9e7325800
Alex James <git@alextjam.es>
2022-10-03 00:18:59 -0500

    Sort packages in overlay.nix

--- overlay.nix
+++ overlay.nix
@@ -1,9 +1,9 @@
 self: super: rec {
+  knlmeanscl = super.callPackage ./knlmeanscl.nix {};
   lsmash = super.callPackage ./lsmash.nix {};
-  vapoursynth-lsmash = super.callPackage ./vapoursynth-lsmash.nix {};
+  vapoursynth-bm3d = super.callPackage ./vapoursynth-bm3d.nix {};
   vapoursynth-fft3dfilter = super.callPackage ./vapoursynth-fft3dfilter.nix {};
   vapoursynth-fmtconv = super.callPackage ./vapoursynth-fmtconv.nix {};
+  vapoursynth-lsmash = super.callPackage ./vapoursynth-lsmash.nix {};
   vapoursynth-nnedi3 = super.callPackage ./vapoursynth-nnedi3.nix {};
-  vapoursynth-bm3d = super.callPackage ./vapoursynth-bm3d.nix {};
-  knlmeanscl = super.callPackage ./knlmeanscl.nix {};
 }
